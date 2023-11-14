3946b2d52dd53319ed26c464847f21ab02fc70e8
Lectre <mhmt.ozcnn@hotmail.com>
2023-11-14 14:45:46 +0300

    I replaced it with DOM

--- script.js
+++ script.js
@@ -1,57 +1,92 @@
+function getComputerChoice(){
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
 
-  function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection){
+  if (
+    (playerSelection === "rock" && computerSelection === "Scissors") ||
+    (playerSelection === "paper" && computerSelection === "Rock") ||
+    (playerSelection === "scissors" && computerSelection === "Paper")
+  )   {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }   else if (
+    (playerSelection === "scissors" && computerSelection === "Rock") ||
+    (playerSelection === "rock" && computerSelection === "Paper") ||
+    (playerSelection === "paper" && computerSelection === "Scissors")
+  )   {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }   else {
+    return "It's a Tie!";
   }
-  
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-  
-    if (
-      (playerSelection === "rock" && computerSelection === "Scissors") ||
-      (playerSelection === "paper" && computerSelection === "Rock") ||
-      (playerSelection === "scissors" && computerSelection === "Paper")
-    ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if (
-      (playerSelection === "scissors" && computerSelection === "Rock") ||
-      (playerSelection === "rock" && computerSelection === "Paper") ||
-      (playerSelection === "paper" && computerSelection === "Scissors")
-    ) {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else {
-      return "It's a Tie!";
-    }
+}
+
+function displayResult(result, computerChoice){
+  let resultDisplay = document.getElementById("resultDisplay");
+  let computerChoiceDisplay = document.getElementById("computerChoiceDisplay");
+  resultDisplay.textContent = result;
+  computerChoiceDisplay.textContent = `Computer Chose: ${computerChoice}`;
+}
+function updateScoreDisplay(){
+  let scoreDisplay = document.getElementById("scoreDisplay");
+  scoreDisplay.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+
+}
+function checkScore(){
+  if (playerScore >= 5){
+    displayResult("Player Wins the Game!")
   }
-  
-  // Play 5 times
-  for (let i = 0; i < 5; i++) {
-    const computerChoice = getComputerChoice();
-    const playerChoice = prompt("Enter Rock, Paper, or Scissors:");
-    console.log("Player chose: ", playerChoice);
-    const result = playRound(playerChoice, computerChoice);
-  
-    console.log(`Computer chose: ${computerChoice}`);
-    if (result.includes('You Win!')) {
-      playerScore++;
-    } else if (result.includes('You Lose!')) {
-      computerScore++;
-    }
-    console.log(result);
-    console.log(`Player Score: ${playerScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-    if(playerScore>computerScore){
-      console.log("Player Win!");
-    }
-    else if (playerScore<computerScore){
-      console.log("Computer Win!");
-    }
-    else{
-      console.log("It's a Tie!")
-    }
+  else if (computerScore >= 5){
+    displayResult("Computer Wins the Game!");
   }
+}
+
+
+function handleClick(playerSelection){
+  const computerChoice = getComputerChoice();
+  const result = playRound(playerSelection, computerChoice);
+  console.log(`Computer chose: ${computerChoice}`);
+  if (result.includes('You Win!')){
+    playerScore++;
+  }
+  else if (result.includes('You Lose!')){
+    computerScore++;
+  }
+  displayResult(result, computerChoice);
+  updateScoreDisplay();
+  checkScore();
   
+}
+
+function resetGame(){
+  playerScore = 0;
+  computerScore = 0;
+  updateScoreDisplay();
+  
+}
+
+function clearDisplay(){
+  let clearcomcho = document.getElementById("computerChoiceDisplay");
+  clearcomcho.textContent = "";
+  let clearresdis = document.getElementById("resultDisplay");
+  clearresdis.textContent = "";
+}
+
+document.getElementById("rockButton").addEventListener("click", function(){
+  handleClick("rock");
+});
+document.getElementById("paperButton").addEventListener("click", function(){
+  handleClick("paper");
+});
+document.getElementById("scissorsButton").addEventListener("click", function(){
+  handleClick("scissors");
+});
+document.getElementById("restartButton").addEventListener("click",function(){
+  resetGame();
+  clearDisplay("computeChoiceDisplay");
+  clearDisplay("resultDisplay");
+});
